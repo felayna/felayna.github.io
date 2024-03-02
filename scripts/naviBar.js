0260f026ac2e7b3a7eb9bd20a1b58d97922bc4ca
felayna <32692270+felayna@users.noreply.github.com>
2024-03-03 00:21:59 +0800

    v1.0.3

--- scripts/naviBar.js
+++ scripts/naviBar.js
@@ -1,14 +1,14 @@
 // 在页面加载完成后执行
 document.addEventListener("DOMContentLoaded", function() {
     // 获取模块容器
-    const container = document.getElementsByClassName("naviBox")[0];
+    const container = document.getElementById("naviBox");
     
     // 创建一个新的 <object> 元素来加载模块
     const objectElement = document.createElement("object");
     objectElement.type = "text/html";
     objectElement.data = "../html/naviBar.html"; // 加载模块文件
     objectElement.width = "100%"; // 设置宽度
-    objectElement.height = "3000px"; // 设置高度
+    objectElement.height = "2500px"; // 设置高度
     objectElement.style.border = "none"; // 设置边框样式
     objectElement.style.overflow = "hidden"; // 设置溢出隐藏
     container.appendChild(objectElement); // 将模块插入容器中
