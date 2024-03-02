fbcbf0cd663d41524612fdaaf1fa3f85a10c70d1
felayna <32692270+felayna@users.noreply.github.com>
2024-03-03 00:11:49 +0800

    Add files via upload

--- scripts/naviBar.js
+++ scripts/naviBar.js
@@ -1,7 +1,7 @@
 // 在页面加载完成后执行
 document.addEventListener("DOMContentLoaded", function() {
     // 获取模块容器
-    const container = document.getElementById("naviBox");
+    const container = document.getElementsByClassName("naviBox")[0];
     
     // 创建一个新的 <object> 元素来加载模块
     const objectElement = document.createElement("object");
